fc79aafb5efa82c8fd2e96be1ee7ebedc2cc34aa
Francois Mora <108535395+francoismora@users.noreply.github.com>
2024-03-30 14:49:59 +0100

    Add mouse wheel keys to navigation layer

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/default/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC, KC_BSLS,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,MO(MAC_FN),MO(L_FN1),KC_RCTL),
+        KC_LCTL,  KC_LALT,  MO(WIN_FN),                             KC_SPACE,                               MO(L_FN1),KC_RALT, KC_RGUI,  KC_RCTL),
 
     [WIN_BASE] = LAYOUT_61_ansi(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_UP] = LAYOUT_61_ansi(
         KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DEL,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,   KC_DEL,   KC_ENT,   _______,  _______,  _______,  _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,             _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BSPC,  KC_DEL,   KC_ENT,   KC_INS,   _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_WH_U,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  KC_WH_D,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,             _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
     };
